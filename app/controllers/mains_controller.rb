3048ee60a2a595cfd622be3a4bbbdd843cfad0d5
Rob Hill <robert.hill0122@gmail.com>
2014-10-03 18:20:55 -0600

    Added show page

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -1,3 +1,6 @@
+require "json"
+require "open-uri"
+
 class MainsController < ApplicationController
   # before_action :gif_info, only: [:create]
 
@@ -16,7 +19,7 @@ class MainsController < ApplicationController
   end
 
   def gif_info(search_term)
-    JSON.parse(open("http://api.gifme.io/v1/search?query=#{search_term}&limit=20&page=0&key=rX7kbMzkGu7WJwvG").read)
+    JSON.parse(open("http://api.gifme.io/v1/search?query=#{search_term}&limit=1&page=0&key=rX7kbMzkGu7WJwvG").read)
   end
 
 end
